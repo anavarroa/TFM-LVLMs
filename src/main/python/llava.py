5d9d1d202bedc4bd86f3009f8754a48bdf11f08f
anavarroa <anavarroa@ita.es>
2024-05-13 12:03:43 +0200

    migración del script fuera del github clonado

---
+++ src/main/python/llava.py
@@ -0,0 +1,65 @@
+# Cargar el modelo pre-entrenado de LLaVA
+from llava.model.builder import load_pretrained_model
+from llava.mm_utils import get_model_name_from_path
+from llava.eval.run_llava import eval_model
+import os
+
+model_path = "liuhaotian/llava-v1.5-7b"
+
+tokenizer, model, image_processor, context_len = load_pretrained_model(
+    model_path=model_path,
+    model_base=None,
+    model_name=get_model_name_from_path(model_path),
+    offload_folder="/content/llava_model"
+)
+
+
+DEEPSPEED_SCRIPT = "deepspeed llava/train/train_mem.py"
+DEEPSPEED_JSON = "./scripts/zero3.json"
+MODEL_NAME = "liuhaotian/llava-v.15-7b"
+DATA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),"..","..","data","dataset.json")
+IMAGE_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)),"..","..","data","imagenes")
+VISION_TOWER = "openai/clip-vit-large-patch14-336"
+OUTPUT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)),"..","..","res")
+
+
+finetune_script = f'''
+{DEEPSPEED_SCRIPT} \
+    --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
+    --deepspeed {DEEPSPEED_JSON} \
+    --model_name_or_path {MODEL_NAME} \
+    --version v1 \
+    --data_path {DATA_PATH} \
+    --image_folder {IMAGE_FOLDER} \
+    --vision_tower {VISION_TOWER} \
+    --mm_projector_type mlp2x_gelu \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --group_by_modality_length True \
+    --bf16 True \
+    --output_dir {OUTPUT_DIR} \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 50000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-4 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True \
+    --report_to wandb
+'''
+
+import torch
+torch.cuda.empty_cache()
