59347a065b8813201a5fb4bcb20d24af603bed1f
anavarroa <anavarroa@ita.es>
2024-06-21 12:44:02 +0200

    codigo funcional

--- src/main/python/evaluation/evaluation.py
+++ src/main/python/evaluation/evaluation.py
@@ -1,17 +1,39 @@
-import subprocess
+import json
+from cider import Cider
+import os
+import numpy as np
 
-def run_llava_serve():
-    command = [
-        "python",
-        "-m",
-        "llava.serve.cli",
-        "--model-path",
-        "liuhaotian/llava-v1.5-7b",
-        "--image-file",
-        "data/imagenes/NWPU/NWPU_images/airplane/airplane_009.jpg",
-        "--load-4bit"
-    ]
-    subprocess.run(command)
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
-if __name__ == "__main__":
-    run_llava_serve()
+# Cargar datos de prueba y predicciones
+with open("results/final.json", 'r') as f:
+    data_test = json.load(f)
+
+with open("/datassd/proyectos/tfm-alvaro/data/sets/data_test.json", 'r') as f:
+    predictions = json.load(f)
+
+# ground truths y resultados
+gts = {}
+res = []
+
+# Formatear los datos de prueba y las predicciones (si no da errores de Assertion o no sé qué)
+for item in data_test:
+    image_id = item['id']
+    gts[image_id] = [conv['value'] for conv in item['conversations'] if conv['from'] == 'gpt']
+
+for item in predictions:
+    image_id = item['id']
+    caption = [conv['value'] for conv in item['conversations'] if conv['from'] == 'gpt'][0]
+    res.append({'image_id': image_id, 'caption': [caption]})
+
+# Aplicar la métrica CIDEr
+cider = Cider()
+score, scores = cider.compute_score(gts, res)
+print(f"CIDEr scores per image: {scores}")
+print(f"CIDEr Score: {score}")
+
+# Filter out zero scores and compute the mean of non-zero scores
+non_zero_scores = [s for s in scores if s != 0]
+mean_non_zero_score = np.mean(non_zero_scores)
+
+print(f"CIDEr non-zero: {mean_non_zero_score}")
